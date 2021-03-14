39d8b89317ce05dc1a472305b454de4396a401e6
javariya <javariyaaamir@ymail.com>
2021-03-14 23:56:14 +0100

    Assignment 2 Task 8

---
+++ Assignment 2/Task 8.js
@@ -0,0 +1,14 @@
+
+
+var userNum = prompt("Enter a number: ");
+
+if (userNum > 0) {
+
+    alert("The number is positive.");
+} else if (userNum < 0) {
+
+    alert (" The number is negative.");
+} else if (userNum == 0 ) {
+
+    alert("The number is 0.");
+} 
