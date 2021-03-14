55b6078e35e42eb6ab5fb0494de98c384998ad7f
javariya <javariyaaamir@ymail.com>
2021-03-14 23:36:45 +0100

    Assignment 2 Task 6

---
+++ Assignment 2/Task6.js
@@ -0,0 +1,8 @@
+var numUser = parseInt(prompt("Enter a number: "));
+
+if (numUser % 2 == 0) {
+
+    alert ("The number you entered is even.");
+} else {
+    alert ("The number you entered is odd.");
+}
