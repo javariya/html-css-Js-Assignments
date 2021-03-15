795b33e17936c957941c39f744b3709427f0ceed
javariya <javariyaaamir@ymail.com>
2021-03-15 13:13:11 +0100

    Assignment 2 Task 9

---
+++ Assignment 2/Task9.js
@@ -0,0 +1,11 @@
+var chUser = prompt ("Enter a character: ");
+
+if (chUser == "a" || chUser == "e" || chUser == "i" || chUser == "o" || chUser == "u") {
+
+    alert ("true");
+} else {
+
+    alert ("false");
+}
+
+
